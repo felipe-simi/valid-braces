f8ff94f91f74c3214fe8933e74a81c3c19cef317
Felipe Miranda <felip.simi@gmail.com>
2020-06-14 15:20:33 -0300

    Preparacao: setup do projeto no Spring Initializr

---
+++ src/main/java/com/simi/validbraces/ValidBracesApplication.java
@@ -0,0 +1,13 @@
+package com.simi.validbraces;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ValidBracesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ValidBracesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/simi/validbraces/ValidBracesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simi.validbraces;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ValidBracesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
